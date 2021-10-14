bb6c34cce62d173c7ea0ab3c4a5bedf5d4c88c65
Christopher Risser <christopher_risser@choicehotels.com>
2021-10-14 13:54:43 -0700

    updating index.html

--- src/Shared/APIService/APIRequest.js
+++ src/Shared/APIService/APIRequest.js
@@ -2,7 +2,7 @@
 
 const getWeatherFromParams = async(userLocation)=>{
     const injectParams = !userLocation.latitude ? `${userLocation.param}=${userLocation.value},us`:`lat=${userLocation.latitude}&lon=${userLocation.longitude}`;
-    const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?${injectParams}&appid=5261ae218e7be91538dfa119b6491587&units=imperial`);
+    const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?${injectParams}&appid=${process.env.REACT_APP_WEATHER_KEY}&units=imperial`);
     const parseResult = await result.json();
     return parseResult;
   
