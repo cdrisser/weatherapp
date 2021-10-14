af7c5cf62bdfb47a6b8ad01b7acfd3fd3531bbf8
cdrisser <48360952+cdrisser@users.noreply.github.com>
2021-10-14 22:36:03 +0000

    Deploying to master from  @ d18e43492dccfc66d8e70be06d7506b7e6d60c8a 🚀

--- src/Shared/ErrorBanner/ErrorBanner.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../ErrorBanner/ErrorBanner.css'
-
-const ErrorBanner = (props)=>{
-    return <p className="error-banner">{props.children}</p>
-}
-
-export default ErrorBanner; 
